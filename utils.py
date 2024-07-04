f8199389e89e3930db547fe22b4612a35e44f5c1
N1cl0ve <wzj13727011813@gmail.com>
2024-07-04 22:10:19 +0800

    Update SetupCallback and instance_from_config

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
 import importlib
+import os
+
+from omegaconf import OmegaConf
+from pytorch_lightning import Callback
 
 
 def get_obj_from_str(string, reload=False):
@@ -13,3 +17,32 @@ def instantiate_from_config(config):
     if not "target" in config:
         raise KeyError("Expected key `target` to instantiate.")
     return get_obj_from_str(config["target"])(**config.get("params", dict()))
+
+
+class SetupCallback(Callback):
+    def __init__(self, now, logdir, ckptdir, cfgdir, config, lightning_config):
+        super().__init__()
+        self.now = now
+        self.logdir = logdir
+        self.ckptdir = ckptdir
+        self.cfgdir = cfgdir
+        self.config = config
+        self.lightning_config = lightning_config
+
+    # 在pretrain例程开始时调用。
+    def on_train_start(self, trainer, pl_module):
+        if trainer.global_rank == 0:
+            # Create logdirs and save configs
+            os.makedirs(self.logdir, exist_ok=True)
+            os.makedirs(self.ckptdir, exist_ok=True)
+            os.makedirs(self.cfgdir, exist_ok=True)
+
+            print("Project config")
+            print(OmegaConf.to_yaml(self.config))
+            OmegaConf.save(self.config,
+                           os.path.join(self.cfgdir, "{}-project.yaml".format(self.now)))
+
+            print("Lightning config")
+            print(OmegaConf.to_yaml(self.lightning_config))
+            OmegaConf.save(OmegaConf.create({"lightning": self.lightning_config}),
+                           os.path.join(self.cfgdir, "{}-lightning.yaml".format(self.now)))
